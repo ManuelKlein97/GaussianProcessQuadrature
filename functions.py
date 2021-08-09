4e3eaf42b2877203b314cd367e1a4219d41c3afb
Manuel Kleinschmager <manuel.kleinschmager@rwth-aachen.de>
2021-08-09 12:10:58 +0200

    Added a get_weights(kernel, X) function to functions.py to get the weights of the GPQ to the given kernel and nodes

--- functions.py
+++ functions.py
@@ -167,3 +167,40 @@ def Optimal_Quadrature_Nodes_Extend(kernel: GPy.kern, NoN_ex: int, NoN_add: int,
         return opt.x, variance
     else:
         return opt.x
+
+def get_weights(kernel: GPy.kern, X: np.ndarray):
+
+    if (kernel.__class__.__name__ == 'PolynomialBasis'):
+        def kernel_function(X, X2):
+            return 1 / (1 - kernel.weight * X * X2)
+        A = np.array([-1, 1])
+
+    if (kernel.__class__.__name__ == 'Trigonometric'):
+        def kernel_function(X, X2):
+            up = 0.5 * (1 - np.square(kernel.weight))
+            down = 1 + np.square(kernel.weight) - 2 * kernel.weight * np.cos(2 * np.pi * (X - X2))
+            return 0.5 + up / down
+        A = np.array([0, 1])
+
+    if (kernel.__class__.__name__ == 'PolynomialBasisFinite'):
+        def kernel_function(X, X2):
+            n = 8
+            upper = (1 - np.power((kernel.weight * X * X2), n + 1))
+            lower = 1 - kernel.weight * X * X2
+            return upper / lower
+        A = np.array([-1, 1])
+
+    n = len(X)
+    u_X = np.zeros(n)
+    K = np.identity(n)
+
+    for i in range(0, n):
+        u_X[i] = sp.integrate.quad(kernel_function, A[0], A[1], X[i])[0]
+        for j in range(0, n):
+            K[i, j] = kernel_function(X[i], X[j])
+            K[j, i] = kernel_function(X[j], X[i])
+
+    K_inverse = np.linalg.pinv(K)
+    weights = np.matmul(K_inverse, u_X)
+
+    return weights
